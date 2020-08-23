120a28d58cea466d5859ed9906f19fef77e4c74d
arazgadir <araz.gadirov@gmail.com>
2020-08-23 13:25:53 +0400

    Cikl1

---
+++ Cikl.2/faktorial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Cikl._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////Вычислить факториал числа n. n! = 1*2*…*n-1*n;!.
+
+
+            //ulong start = 1;
+
+            //ulong enter = ulong.Parse(Console.ReadLine());                  // введение числа 
+
+            //for (ulong i = 1; i <= enter; i++)
+            //    start *= i;
+
+            //{
+
+            //    Console.WriteLine("factorial= " + start);
+            // }
+            
+
+
+            // FIBONACCI
+
+
+            int a = 0;
+            int b = 1;
+            int c = 0;
+           
+
+            int[] fib = new int [30];
+
+            for (int i = 0; i <fib.Length ; i++)
+            {
+
+                c = a + b;
+
+                a = b;
+                b = c;
+                Console.WriteLine(b);
+            }
+
+            
+        }
+    }
+}
